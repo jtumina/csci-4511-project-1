3b3def5e9e83b51ecea67b13e5b463cac08ad03c
Jack Umina <jumina@gwu.edu>
2021-02-12 13:12:58 -0500

    Optimized ucs_uninformed_search to halt search once dst is found

--- src/graph.py
+++ src/graph.py
@@ -38,6 +38,12 @@ class Graph:
             s += str(self.nodes[v])
         return s
 
+    """
+    Finds matching tuple and decreases priority.
+    Assumes each element is a tuple formated as such:
+        k(<priority>, <vertex_id>, <vertex_obj>)
+    If tuple is not found, returns false
+    """
     def decrease_priority(queue, v, new_prio):
         for i in range(0, len(queue)):
             if queue[i][1] == v.id:
@@ -46,57 +52,54 @@ class Graph:
 
         return False
 
-    def dijkstra_uninformed_search(self, src, dst):
+    def ucs_uninformed_search(self, src, dst):
         # Check if src and dst exist in graph
         if src not in self.nodes or dst not in self.nodes:
             return None
 
+        # Get the vertex object of src
+        src = self.nodes[src]
+
         prev = {}
+        prev[src] = None
         dist = {}
-        dist[self.nodes[src]] = 0
+        dist[src] = 0
+        num_explored_nodes = 0
+        frontier = [] # Priority queque to maintain closed list
+        visited = [] # List to track opened nodes 
+        found = False
 
-        # Priority queque
-        q = []
+        # Each element of the queue is a 3 tuple.
+        # The vertex id is neccessary to allow heapq to properly sort 
+        # when multiple elementes share the same priority.
+        heapq.heappush(frontier, (dist[src], src.id, src))
 
-        for v in self.nodes.values():
-            if v.id != src:
-                # Distance between src and v is unknown
-                dist[v] = math.inf
-                prev[v] = None
-
-            # Each element of the queue is a 3 tuple.
-            # The vertex id is neccessary to allow heapq to properly sort 
-            # when multiple elementes share the same priority.
-            heapq.heappush(q, (dist[v], v.id, v))
-
-        while q:
+        while not found and frontier:
             # Get the neighbor with least cost
-            v = heapq.heappop(q)[2]
+            v = heapq.heappop(frontier)[2]
+            num_explored_nodes += 1
 
-            # For each neighbor of v, check if there's a shorter path
-            for u in v.adj: 
+            if v.id == dst:
+                found = True
+
+            visited.append(v)
+
+            for u in v.adj:
                 new_cost = dist[v] + u.adj[v]
 
-                if new_cost < dist[u]:
+                if u not in dist or new_cost < dist[u]:
                     dist[u] = new_cost
                     prev[u] = v
-                    Graph.decrease_priority(q, u, new_cost)
 
-        # Construct the shortest path between src and dst
-        path = []
-        path_length = 0
+                    # If decrease_priority returns False, u is not in frontier
+                    # so it must be added if it has not been visited
+                    in_frontier = Graph.decrease_priority(frontier, u, new_cost)
+                    if not in_frontier and u not in visited:
+                        prev[u] = v
+                        heapq.heappush(frontier, (dist[u], u.id, u))
 
-        v = self.nodes[dst]
-        path.append(v.id)
-
-        while v.id != src:
-            u = prev[v]
-            path.append(u.id)
-            path_length += int(v.adj[u])
-            v = u
-
-        path.reverse() 
-        ret = {"path": path, "path_length": path_length}
+        ret = self.construct_path(src, dst, prev) 
+        ret["num_explored_nodes"] = num_explored_nodes
 
         return ret 
 
@@ -108,59 +111,83 @@ class Graph:
         dx = abs(dst.square_id % 10 - v.square_id % 10)
         dy = abs(dst.square_id // 10 - v.square_id // 10)
 
-        return dx + dy
+        return (dx**2 + dy**2)**0.5
 
     def a_star_informed_search(self, src, dst):
-         # Check if src and dst exist in graph
+        # Check if src and dst exist in graph
         if src not in self.nodes or dst not in self.nodes:
             return None
 
-        prev = {}
-        dist = {}
-        dist[self.nodes[src]] = 0
+        # Get the vertex object of src
+        src = self.nodes[src]
 
-        # Priority queque
-        q = []
+        prev = {} # Map to track a nodes previous in the path
+        prev[src] = None
+        dist = {} # Map to track a nodes distance to src so far
+        dist[src] = 0
+        frontier = [] # Priority queue to maintain closed list
+        visited = [] # List to track opened nodes
+        found = False
+        num_explored_nodes = 0
 
-        for v in self.nodes.values():
-            if v.id != src:
-                # Distance between src and v is unknown
-                dist[v] = math.inf
-                prev[v] = None
+        # Each element of the queue is a 3 tuple.
+        # The vertex id is neccessary to allow heapq to properly sort 
+        # when multiple elementes share the same priority.
+        heapq.heappush(frontier, (dist[src], src.id, src))
 
-            # Each element of the queue is a 3 tuple.
-            # The vertex id is neccessary to allow heapq to properly sort 
-            # when multiple elementes share the same priority.
-            heapq.heappush(q, (dist[v], v.id, v))
+        # While we haven't reached dst and there are still nodes to explore in
+        # the frontier
+        while not found and frontier:
+            # Get the node with least cost
+            v = heapq.heappop(frontier)[2]
+            num_explored_nodes += 1
 
-        while q:
-            # Get the neighbor with least cost
-            v = heapq.heappop(q)[2]
+            if v.id == dst:
+                found = True
+        
+            visited.append(v)
 
             # For each neighbor of v, check if there's a shorter path
             for u in v.adj: 
                 new_cost = dist[v] + u.adj[v]
 
-                if new_cost < dist[u]:
+                if u not in dist or new_cost < dist[u]:
                     dist[u] = new_cost
                     prev[u] = v
-                    priority = new_cost + self.heuristic(u, self.nodes[dst])
-                    Graph.decrease_priority(q, u, priority)
 
-        # Construct the shortest path between src and dst
+                    # If decrease_priority returns False, u is not in frontier 
+                    # so it must be added if it has not been visited
+                    priority = new_cost + self.heuristic(u, self.nodes[dst])
+                    in_frontier = Graph.decrease_priority(frontier, u, priority)
+                    if not in_frontier and u not in visited:
+                        prev[u] = v
+                        heapq.heappush(frontier, (priority, u.id, u))
+
+        ret = self.construct_path(src, dst, prev) 
+        ret["num_explored_nodes"] = num_explored_nodes
+
+        return ret 
+
+    # Construct the shortest path and return as list
+    def construct_path(self, src, dst, prev):
         path = []
         path_length = 0
 
         v = self.nodes[dst]
         path.append(v.id)
 
-        while v.id != src:
+        while prev[v] and v.id != src:
             u = prev[v]
             path.append(u.id)
             path_length += int(v.adj[u])
             v = u
 
         path.reverse() 
-        ret = {"path": path, "path_length": path_length}
+        
+        ret = {
+                "path": path,
+                "path_length": path_length
+              }
 
         return ret 
+
